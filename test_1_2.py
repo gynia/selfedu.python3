4b261ae657d52f0ec40a9e7a53d31ff368b11e7f
gynia <auramenka87@gmail.com>
2024-03-09 23:36:36 +0300

    change file test_1_2.py, lesson_1_3.py. and add file lesson_1_4

--- test_1_2.py
+++ test_1_2.py
@@ -46,3 +46,51 @@ class Dictionary:
 
 print(getattr(Dictionary, 'rus_word', False))
 
+
+class TravelBlog:
+    total_blogs = 0
+
+
+tb1 = TravelBlog()
+tb1.name = "Франция"
+tb1.days = 6
+TravelBlog.total_blogs += 1
+
+tb2 = TravelBlog()
+tb2.name = "Италия"
+tb2.days = 5
+TravelBlog.total_blogs += 1
+
+
+class Figure:
+    type_fig = 'ellipse'
+    color = 'red'
+
+
+fig1 = Figure()
+fig1.start_pt = (10, 5)
+fig1.end_pt = (100, 20)
+fig1.color = 'blue'
+
+
+del fig1.color
+
+for key in fig1.__dict__:
+    print(key, end=" ")
+    print()
+
+
+class Person:
+    name = 'Сергей Балакирев'
+    job = 'Программист'
+    city = 'Москва'
+
+
+p1 = Person()
+p1.job = Person.job
+print(True) if "job" in p1.__dict__ else print(False)
+print(p1.__dict__)
+print(Person.__dict__)
+
+
+
