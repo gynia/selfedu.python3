da6b9b74e7e84a4806cc208597347fc948c39053
gynia <auramenka87@gmail.com>
2024-03-11 00:14:37 +0300

    add link leson_1_4

--- lesson_1_4.py
+++ lesson_1_4.py
@@ -1,7 +1,7 @@
 #!/user/bin/python3.8           1.4 Методы классов. Параметр self
 # -*- coding: utf-8 -*-
 
-# link  https://proproprogs.ru/python_oop/metody-klassov-parametr-self
+# link https://proproprogs.ru/python_oop/metody-klassov-parametr-self
 
 
 # Класс может содержать свойства (данные) и методы (функции). Благодаря 
