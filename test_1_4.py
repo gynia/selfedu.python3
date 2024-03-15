c8302a59a6320a1ca609f508c92b4bd66be61c6f
gynia <auramenka87@gmail.com>
2024-03-15 23:27:59 +0300

    changes test_1_4.py

--- test_1_4.py
+++ test_1_4.py
@@ -1,5 +1,30 @@
 #!/user/bin/python3.11
 # -*- coding: utf-8 -*-
 
+class MediaPlayer:
+    filename = None
+
+    def open(self, file):
+        self.filename = file
+
+    def play(self):
+        print(f"Воспроизведение {self.filename}")
+
+
+media1 = MediaPlayer()
+media2 = MediaPlayer()
+
+media1.open("filemedia1")
+media2.open("filemedia2")
+
+media1.play()
+media2.play()
+
+
+
+class Graph:
+
+
+
 
 
