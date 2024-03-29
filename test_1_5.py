284a0cad66703a23578a083634b1b7c8a5d74a31
gynia <auramenka87@gmail.com>
2024-03-29 16:09:45 +0300

    change test_1_5.py

--- test_1_5.py
+++ test_1_5.py
@@ -1,6 +1,6 @@
 #!/user/bin/python3.11
 # -*- coding: utf-8 -*-
-
+import random
 
 class Money:
     def __init__(self, money=0):
@@ -34,5 +34,55 @@ p1 = Point(10, 20)
 p2 = Point(12, 5, 'red')
 
 
+class Line:
+    def __init__(self, a, b, c, d):
+        self.sp = (a, b)
+        self.ep = (c, d)
 
 
+class Rect:
+    def __init__(self, a, b, c, d):
+        self.sp = (a, b)
+        self.ep = (c, d)
+
+
+class Ellipse:
+    def __init__(self, a, b, c, d):
+        self.sp = (a, b)
+        self.ep = (c, d)
+
+
+elements = []
+for i in range(217):
+    g = None
+    rand = random.choice([1, 2, 3])
+    a = random.randint(0, 10)
+    b = random.randint(0, 10)
+    c = random.randint(0, 10)
+    d = random.randint(0, 10)
+    if rand == 1:
+        g = Line(0, 0, 0, 0)
+    elif rand == 2:
+        g = Rect(a, b, c, d)
+    elif rand == 3:
+        g = Ellipse(a, b, c, d)
+    elements.append(g)
+
+
+# print(elements)
+# print(len(elements))
+#
+# for i in elements:
+#     if isinstance(i, Line):
+#         print(type(i))
+#         print(i.ep, i.sp)
+#
+
+
+class TriangleChecker:
+    def __init__(self):
+        pass
+
+
+
+tr = TriangleChecker(a, b, c)
