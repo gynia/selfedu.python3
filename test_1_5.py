37efc9a421b7640d809e8988af26d070a1125a39
gynia <auramenka87@gmail.com>
2024-04-07 15:17:23 +0300

    change test_1_5.py add class is_triangle

--- test_1_5.py
+++ test_1_5.py
@@ -2,15 +2,16 @@
 # -*- coding: utf-8 -*-
 import random
 
+
 class Money:
     def __init__(self, money=0):
         self.money = money
 
 
-my_money = Money(100)
-your_money = Money(1000)
-print(my_money.money)
-print(your_money.money)
+# my_money = Money(100)
+# your_money = Money(1000)
+# print(my_money.money)
+# print(your_money.money)
 
 
 class Point:
@@ -27,11 +28,11 @@ for i in range(1, 2000, 2):
     if i == 3:
         p.color = "yellow"
 
-print(points)
-print(len(points))
-print(points[1].color)
-p1 = Point(10, 20)
-p2 = Point(12, 5, 'red')
+# print(points)
+# print(len(points))
+# print(points[1].color)
+# p1 = Point(10, 20)
+# p2 = Point(12, 5, 'red')
 
 
 class Line:
@@ -80,9 +81,53 @@ for i in range(217):
 
 
 class TriangleChecker:
-    def __init__(self):
-        pass
+
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def is_triangle(self):
+        test_li = []
+        if type(self.a) in (int, float) and self.a > 0:
+            test_li.append(self.a)
+        if type(self.b) in (int, float) and self.b > 0:
+            test_li.append(self.b)
+        if type(self.c) in (int, float) and self.c > 0:
+            test_li.append(self.c)
+        test_li.sort()
+        print(test_li)
+        if len(test_li) != 3:
+            return 1
+        if test_li[-1] >= sum(test_li[0:2]):
+            return 2
+        return 3
+
+##
+# tr = TriangleChecker(1, 1, 2)
+# print(tr.is_triangle())
+#
+# tr = TriangleChecker(2, 2, 2)
+# print(tr.is_triangle())
+#
+# tr = TriangleChecker(1, "1", 2)
+# print(tr.is_triangle())
+#
+# tr = TriangleChecker(-1, "1", 2)
+# print(tr.is_triangle())
+#
+# tr = TriangleChecker(3, 4, 5)
+# print(tr.is_triangle())
+#
+# tr = TriangleChecker('3', 4, 5)
+# print(tr.is_triangle())
+#
+# tr = TriangleChecker(3, 4.4, 5)
+# print(tr.is_triangle())
+#
+# tr = TriangleChecker(3, 0, 5)
+# print(tr.is_triangle())
 
 
 
-tr = TriangleChecker(a, b, c)
+
