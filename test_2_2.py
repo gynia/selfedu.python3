dfa7ca2886f7032984a640077a1ea5c78f6f98a5
gynia <auramenka87@gmail.com>
2024-04-29 23:38:05 +0300

    change file: lesson_2_2.py and add class Car in test_2_2.py

--- test_2_2.py
+++ test_2_2.py
@@ -1,6 +1,35 @@
 #!/user/bin/python3.11
 # -*- coding: utf-8 -*-
 
+class Car:
+    def __init__(self, model=None):
+        if self.validate(model) or model is None:
+            self.__model = model
+
+    @staticmethod
+    def validate(model: str):
+        return True if (type(model) in (str,)
+                        and 100 >= len(model) >= 2) else False
+
+    @property
+    def model(self):
+        return self.__model
+
+    @model.setter
+    def model(self, model):
+        if self.validate(model):
+            self.__model = model
+
+
+car = Car()
+print(car.model)
+print(car.__dict__)
+car.model = "Toyota"
+print(car.model)
+print(car.validate("a"))
+print(car.validate(45))
+print(car.__dict__)
+
 
 
 
