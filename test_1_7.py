81cb5b71e337d0c43f18bfd83aa5026f490c0232
gynia <auramenka87@gmail.com>
2024-04-30 20:28:27 +0300

    Revert "Revert "add class Factory in test_1_7.py""
    This reverts commit ee99112c4142f1177b1bf51c97d50556d81e0c32.

--- test_1_7.py
+++ test_1_7.py
@@ -1,5 +1,29 @@
 #!/user/bin/python3.11
 # -*- coding: utf-8 -*-
 
+class Factory:
+    @staticmethod
+    def build_sequence():
+        return list()
+
+    @staticmethod
+    def build_number(text):
+        return int(text)
 
 
+class Loader:
+    @staticmethod
+    def parse_format(string, factory):
+        seq = factory.build_sequence()
+        for sub in string.split(","):
+            item = factory.build_number(sub)
+            seq.append(item)
+
+        return seq
+
+
+res_1 = Loader.parse_format("4, 5, -6", Factory)
+res_2 = Loader.parse_format("1, 2, 3, -5, 10", Factory)
+
+print(res_1)
+print(res_2)
