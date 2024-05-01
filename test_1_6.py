b3b45392bdc2275baf75c9687dc09a8f196c2103
gynia <auramenka87@gmail.com>
2024-05-01 15:02:56 +0300

    add class Dialog in file test_1_6.py

--- test_1_6.py
+++ test_1_6.py
@@ -17,10 +17,10 @@ class SingletonFive():
     is_enumerate = True
 
     def __new__(cls, *args, **kwargs):
-        #print("func __new__")
+        # print("func __new__")
         if cls.count < 5:
             cls.__instance = super().__new__(cls)
-            #print(cls.count)
+            # print(cls.count)
             cls.count += 1
         else:
             cls.is_enumerate = False
@@ -29,10 +29,10 @@ class SingletonFive():
     def __init__(self, *args, **kwargs):
         if self.is_enumerate:
             self.__setattr__("name", *args)
-        #print("func __init__")
-        #print(self.__dict__)
-        #print(self.__instance)
-        #print(id(self))
+        # print("func __init__")
+        # print(self.__dict__)
+        # print(self.__instance)
+        # print(id(self))
 
     def __del__(self):
         self.__instance = None
@@ -41,6 +41,54 @@ class SingletonFive():
 
 
 objs = [SingletonFive(str(n)) for n in range(10)]
-#print(objs)
-#print(len(objs))
+
+
+# print(objs)
+# print(len(objs))
+
+
+class DialogWindows:
+    name_class = "DialogWindows"
+
+
+class DialogLinux:
+    name_class = "DialogLinux"
+
+
+class Dialog():
+    def __new__(cls, *args, **kwargs):
+        if TYPE_OS == 1:
+            dlg = DialogWindows()
+        else:
+            dlg = DialogLinux()
+        dlg.name = str(*args)
+        return dlg
+
+
+# TYPE_OS = 1    # 1 - Windows; 2 - Linux
+# di = Dialog()
+# print(di.name)
+# print(type(di))
+# print(di.__dict__)
+#
+# dlg_1 = Dialog("name_1")
+# print(dlg_1.name)
+# print(type(dlg_1))
+# print(dlg_1.__dict__)
+#
+# TYPE_OS = "34"    # 1 - Windows; 2 - Linux
+# dlg_2 = Dialog("name_2")
+# print(dlg_2.name)
+# print(type(dlg_2))
+# print(dlg_2.__dict__)
+#
+# di = Dialog(3)
+# print(di.name)
+# print(type(di))
+# print(di.__dict__)
+#
+# print(isinstance(dlg_1, DialogWindows))
+# print(isinstance(dlg_2, DialogLinux))
+
+
 
