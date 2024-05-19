bd62163a798075418bc88685df415805b7ba001e
EgAvr <auramenka87@gmail.com>
2024-05-19 23:08:17 +0300

    add class Book in file test_2_1.py

--- test_2_1.py
+++ test_2_1.py
@@ -101,3 +101,33 @@ class Money:
 # m2 = mn_2.get_money()    # 120
 # print(m2)
 
+class Book:
+    def __init__(self, author, title, price):
+        self.__author = author
+        self.__title = title
+        self.__price = price
+
+    def set_author(self, author):
+        self.__author = author
+
+    def get_author(self):
+        return self.__author
+
+    def set_title(self, title: str):
+        self.__title = title
+
+    def get_title(self):
+        return self.__title
+
+    def set_price(self, price):
+        self.__price = price
+
+    def get_price(self):
+        return self.__price
+
+
+# book = Book("автор", "название", "цена")
+# print(book.get_author())
+# print(book.get_title())
+# print(book.get_price())
+
